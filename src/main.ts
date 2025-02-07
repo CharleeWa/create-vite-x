93dcfd00ab6f68b31b998a43f27c2b481d76ff65
CharleeWa <18888351756@163.com>
2025-02-07 10:55:11 +0800

    chore: update deps

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
-import { createHead } from '@unhead/vue'
 import App from '@/App.vue'
 import router from '@/router'
+import { createHead } from '@unhead/vue'
+import { createApp } from 'vue'
 import '@/styles/app.css'
 
 const app = createApp(App)
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_APP_PUBLIC_PATH),
+  routes,
 })
 
 router.beforeEach((_to, _from, next) => {
